fd5fe1d795d2186265bccf238eb090f3a76557d0
Hannes Forsgård <hannes.forsgard@fripost.org>
2018-09-28 13:55:04 +0200

    Start on propper plugin style

---
+++ plugin.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace byrokrat\giroappmailer;
+
+use byrokrat\giroapp\Plugin\PluginInterface;
+use byrokrat\giroapp\Plugin\EnvironmentInterface;
+use Pimple\Container;
+
+return new class implements PluginInterface {
+    public function loadPlugin(EnvironmentInterface $env): void
+    {
+        $container = new Container;
+
+        $container->register(new DependenciesProvider);
+
+        $container['smtp_string'] = $env->readConfig('mailer_smtp_string');
+        $container['template_dir'] = $env->readConfig('mailer_template_dir');
+        $container['queue_dir'] = $env->readConfig('mailer_queue_dir');
+
+        $env->registerCommand($container[MailerSendCommand::CLASS]);
+        $env->registerCommand($container[MailerStatusCommand::CLASS]);
+        $env->registerSubscriber($container[MailingSubscriber::CLASS]);
+    }
+};
